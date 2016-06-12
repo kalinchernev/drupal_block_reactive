ee75b4767b8535ccfc5557f05d96d57d69595a6a
Kalin Chernev <kalin.chernev@gmail.com>
2016-06-12 13:16:30 +0200

    Scaffolding components.

--- js/dist/index.js
+++ js/dist/index.js
@@ -1,20 +1,73 @@
-"use strict";
+'use strict';
 
 Drupal.behaviors.drupal_block_reactive = {
   attach: function attach(context) {
 
     // CommentBox component definition.
     var CommentBox = React.createClass({
-      displayName: "CommentBox",
+      displayName: 'CommentBox',
+
+      getInitialState: function getInitialState() {
+        return { data: [] };
+      },
+      render: function render() {
+        return React.createElement(
+          'div',
+          { className: 'commentBox' },
+          React.createElement(CommentList, null)
+        );
+      }
+    });
+
+    // CommentList component definition.
+    var CommentList = React.createClass({
+      displayName: 'CommentList',
+
+      render: function render() {
+        var tempComments = [{
+          title: 'INTT-1',
+          date: Date(),
+          key: 1
+        }, {
+          title: 'INTT-2',
+          date: Date(),
+          key: 2
+        }];
+
+        // List of comments to render.
+        var commentNodes = tempComments.map(function (comment) {
+          return React.createElement(
+            Comment,
+            { title: comment.title, key: comment.key },
+            comment.title
+          );
+        });
+
+        return React.createElement(
+          'div',
+          { className: 'commentList' },
+          commentNodes
+        );
+      }
+    });
+
+    // Comment component definition.
+    var Comment = React.createClass({
+      displayName: 'Comment',
 
       render: function render() {
         return React.createElement(
-          "div",
-          { className: "commentBox" },
+          'div',
+          { className: 'comment' },
           React.createElement(
-            "p",
-            null,
-            "Drupal behaviors dependency."
+            'span',
+            { className: 'commentTitle' },
+            'title placeholder'
+          ),
+          React.createElement(
+            'span',
+            { className: 'commentDate' },
+            'date placeholder'
           )
         );
       }
--- js/src/index.jsx
+++ js/src/index.jsx
@@ -3,10 +3,58 @@ Drupal.behaviors.drupal_block_reactive = {
 
     // CommentBox component definition.
     const CommentBox = React.createClass({
+      getInitialState: () => {
+        return {data: []};
+      },
       render: () => {
         return (
           <div className="commentBox">
-            <p>Drupal behaviors dependency.</p>
+            <CommentList />
+          </div>
+        );
+      }
+    });
+
+    // CommentList component definition.
+    const CommentList = React.createClass({
+      render: () => {
+        const tempComments = [
+          {
+            title: 'INTT-1',
+            date: Date(),
+            key: 1
+          },
+          {
+            title: 'INTT-2',
+            date: Date(),
+            key: 2
+          }
+        ];
+
+        // List of comments to render.
+        const commentNodes = tempComments.map((comment) => {
+          return (
+            <Comment title={comment.title} key={comment.key}>
+              {comment.title}
+            </Comment>
+          );
+        });
+
+        return (
+          <div className="commentList">
+            {commentNodes}
+          </div>
+        );
+      }
+    });
+
+    // Comment component definition.
+    const Comment = React.createClass({
+      render: () => {
+        return (
+          <div className="comment">
+            <span className="commentTitle">title placeholder</span>
+            <span className="commentDate">date placeholder</span>
           </div>
         );
       }
